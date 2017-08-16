69e5dfa5770f36ec2b445a80ea711342e42fe1ff
sjnov11 <sjnov11@gmail.com>
2017-08-17 01:12:55 +0900

    t1

--- py/CaffeFunction.py
+++ py/CaffeFunction.py
@@ -45,6 +45,7 @@ class CaffeFunction(DeepFunction):
         l_blob.append(self.weight.get_blob(str(l.name), idx))
       
       if l.type == "Python" and l.python_param.layer == "ProposalLayer":
+        print(Proposal(l, l_blob, self))
         self.ops.append(Proposal(l, l_blob, self))
       else:
         self.ops.append(str2class(l.type)(l, l_blob, self))
