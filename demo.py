db94bd3d322cbebc593905d6bfbc4ccd8cee149e
sjnov11 <sjnov11@gmail.com>
2017-08-16 22:27:47 +0900

    m3

--- demo.py
+++ demo.py
@@ -52,6 +52,7 @@ NETS = {'vgg16': ('VGG16',
 def vis_detections(im, class_name, dets, thresh=0.5):
     """Draw detected bounding boxes."""
     inds = np.where(dets[:, -1] >= thresh)[0]
+    print(inds)
     if len(inds) == 0:
         return
 
@@ -102,7 +103,7 @@ def demo(net, image_name):
            '{:d} object proposals').format(timer.total_time, boxes.shape[0])
     
     # Visualize detections for each class
-    CONF_THRESH = 0.1
+    CONF_THRESH = 0.8
     NMS_THRESH = 0.3
     for cls_ind, cls in enumerate(CLASSES[1:]):
         cls_ind += 1 # because we skipped background
@@ -154,7 +155,7 @@ if __name__ == '__main__':
 
     print '\n\nLoaded network {:s}'.format(caffemodel)
 
-    im_names = [ '000001.jpg', '000002.jpg', '000456.jpg', '001141.jpg', '004545.jpg']
+    im_names = [ '000001.jpg', '000002.jpg', '000456.jpg']#, '001141.jpg', '004545.jpg']
     for im_name in im_names:
         print '~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~'
         print 'Demo for {}'.format(im_name)
